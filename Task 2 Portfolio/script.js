95da239f33cfa23ee61f01d3b1951555f2d22466
Fakhir Shaukat <fakhirshaukat8@gmail.com>
2025-02-21 21:44:41 +0500

    Add files via upload

---
+++ Task 2 Portfolio/script.js
@@ -0,0 +1,18 @@
+const btnCv = document.getElementById('btn2');
+btnCv.addEventListener('click',function(){
+
+    window.location.href='Fakhir Shaukat CV.pdf'
+
+});
+
+const home = document.getElementById("#logoid");
+    home.addEventListener('click', function(){
+
+        window.location.href="index.html";
+
+    });
+
+
+
+
+
